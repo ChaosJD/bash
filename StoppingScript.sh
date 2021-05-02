91e9435e7b66fab2ffd969f4b5997cdf2aa822a2
ChaosJD <32511119+ChaosJD@users.noreply.github.com>
2021-05-02 17:34:21 +0200

    delete commit in file

--- StoppingScript.sh
+++ StoppingScript.sh
@@ -2,4 +2,3 @@
 #Stops Gesture Recognition with -SIGTERM (15) to end / terminate the process
 STOPPINGPROCESS=
 kill $STOPPINGPROCESS
-#kill $(ps aux | grep "python3 main.py" | head -n 1 | cut -d ' ' -f 8)
