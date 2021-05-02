722b74edecd5b7617d9d73d0f529870ae51c48a6
ChaosJD <32511119+ChaosJD@users.noreply.github.com>
2021-05-02 17:33:29 +0200

    delete Process ID
    from file

--- StoppingScript.sh
+++ StoppingScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Stops Gesture Recognition with -SIGTERM (15) to end / terminate the process
-STOPPINGPROCESS='8819'
+STOPPINGPROCESS=
 kill $STOPPINGPROCESS
 #kill $(ps aux | grep "python3 main.py" | head -n 1 | cut -d ' ' -f 8)
